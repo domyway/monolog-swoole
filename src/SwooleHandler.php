10c251fa7af57280953d144bc546594a1ead3cb6
domyway <weizhao029@gmail.com>
2016-06-24 18:24:11 +0800

    first for testing

---
+++ src/SwooleHandler.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace SwooleHandler;
+
+use Monolog\Logger;
+use Monolog\Handler\AbstractProcessingHandler;
+use WebSocket\Client;
+
+class SwooleHandler extends AbstractProcessingHandler 
+{
+    static $client;
+    static $ws;
+
+    public function __construct(array $config)
+    {
+        self::$ws = $config['ws'];
+    }
+
+    static public function getInstance()
+    {
+        if(self::$client)
+        {
+            self::$client = new Client(self::$ws);
+        }
+
+        return self::$client;
+    }
+
+    public function write(array $record)
+    {
+        return self::getInstance()->send(json_encode($record, JSON_UNESCAPED_UNICODE));
+    }
+
+    public function receive()
+    {
+        return self::getInstance()->receive();
+    }
+}
+
---
+++ src/realTimeLogServer.php
@@ -0,0 +1,17 @@
+<?php
+$ws = new swoole_websocket_server("0.0.0.0", 9600);
+
+$ws->on('open', function ($ws, $request) {
+    $ws->push($request->fd, "hello, welcome\n");
+});
+
+$ws->on('message', function ($ws, $frame) {
+    echo "Message: {$frame->data}\n";
+    $ws->push($frame->fd, "server: {$frame->data}");
+});
+
+$ws->on('close', function ($ws, $fd) {
+    echo "client-{$fd} is closed\n";
+});
+
+$ws->start();
